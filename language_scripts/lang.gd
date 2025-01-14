d4ab43713b84a95ff29dbf9ffa01e670597a0972
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2025-01-14 18:26:15 +1300

    Be less demanding about how to format numbers

--- language_scripts/lang.gd
+++ language_scripts/lang.gd
@@ -204,7 +204,7 @@ func tokenize_code(text: String) -> Array[Token]:
 			next_type = [Token.Type.PARAMETER]
 			working_token = ""
 			continue
-		
+			
 		elif chr == ")":
 			if not working_token.is_empty():
 				tokenized_code.append(_number_parameter(working_token))
@@ -212,6 +212,25 @@ func tokenize_code(text: String) -> Array[Token]:
 			working_token = ""
 			continue
 			
+		elif OPERATORS.has(chr):
+			if working_token.is_valid_int():
+				if next_type.has(Token.Type.PARAMETER):
+					tokenized_code.append(Token.new(working_token, [Token.Type.INT, Token.Type.PARAMETER]))
+				else:
+					tokenized_code.append(Token.new(working_token, [Token.Type.INT]))
+				
+			elif working_token.is_valid_float():
+				if next_type.has(Token.Type.PARAMETER):
+					tokenized_code.append(Token.new(working_token, [Token.Type.FLOAT, Token.Type.PARAMETER]))
+				else:
+					tokenized_code.append(Token.new(working_token, [Token.Type.FLOAT]))
+			
+			tokenized_code.append(Token.new(chr, [Token.Type.OPERATOR]))
+			
+			next_type = [Token.Type.INT, Token.Type.FLOAT]
+			working_token = ""
+			continue
+		
 		working_token += chr
 		print(working_token,"   ",tokenized_code,"   ",next_type,"    ",is_comment)
 	
