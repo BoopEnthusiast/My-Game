61d94b51a6a7559e770f002a9afacc9e7d79df4c
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2025-12-17 01:45:50 +1300

    Basic game

---
+++ globals/nodes.gd
@@ -0,0 +1,5 @@
+extends Node
+
+
+var player: Player
+var world_manager: WorldManager
--- worlds/world_manager.gd
+++ worlds/world_manager.gd
@@ -1,2 +1,41 @@
 class_name WorldManager
 extends Node
+
+
+var mouse_captured: bool = false
+
+@onready var node_graph: Control = $NodeGraph
+
+
+func _enter_tree() -> void:
+	Nodes.world_manager = self
+
+
+func _input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed(&"ui_cancel"):
+		if mouse_captured:
+			release_mouse()
+		else:
+			capture_mouse()
+	
+	if Input.is_action_just_pressed(&"open_code"):
+		if get_tree().paused:
+			get_tree().paused = false
+			node_graph.visible = false
+		else:
+			get_tree().paused = true
+			node_graph.visible = true
+
+
+func _ready() -> void:
+	capture_mouse()
+
+
+func capture_mouse() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	mouse_captured = true
+
+
+func release_mouse() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	mouse_captured = false
